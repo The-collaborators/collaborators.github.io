fadbf831416404b60ac6d2367107c98fdc1d6be8
Anoushka Palvia <03anoushkapalvia@gmail.com>
2020-12-01 10:32:15 +0530

    routes created

--- app.js
+++ app.js
@@ -2,75 +2,26 @@
 const express= require("express");
 const ejs=require("ejs");
 const app=express();
-const clientID="ea870420606678b49849";
-const clientSecret="80b21123dec51d57d458e044235c68ea659e636e";
-const cookieParser = require('cookie-parser');
 const session      = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
-var store=new MongoDBStore( require("./config/database"));
 const bodyParser = require('body-parser')
 var methodOverride = require('method-override');
 var GitHubStrategy = require('passport-github2').Strategy;
 var partials = require('express-partials');
 var passport = require('passport');
-const User = require("./models/user");
 const mongoose=require("mongoose");
-// require('dotenv').config()
+require('dotenv').config()
 
-// console.log(process.env);
+// requiring from different files
+var store=new MongoDBStore( require("./config/database"));
+//const User = require("./models/user");
+const users=require("./routes/users");
 
-passport.serializeUser(function(user, done) {
-  //console.log(user.username);
-  done(null, user.username);
-});
+//env variables
+const clientID=process.env.CLIENT_ID;
+const clientSecret=process.env.CLIENT_Secret;
 
-passport.deserializeUser(function(user, done) {
-  //console.log(user);
-  User.findOne({userName:user},function(err,user){
-    done(null, user);
-  });
-  
-});
-
-passport.use(new GitHubStrategy({
-  clientID: clientID,
-  clientSecret: clientSecret,
-  callbackURL: "http://localhost:3000/signin"
-},
-function(access_token, refreshToken, profile, done) {
-
-  // asynchronous verification, for effect...
-  process.nextTick(function () {
-    
-    // To keep the example simple, the user's GitHub profile is returned to
-    // represent the logged-in user.  In a typical application, you would want
-    // to associate the GitHub account with a user record in your database,
-    // and return that user instead.
-    //console.log(User);
-    User.findOne({userName:profile.username},function(err,foundUser){
-      if(foundUser!=null){
-        //console.log(foundUser);
-        //return done(err, foundUser);
-      }
-      else{
-        let newUser = new User({
-          email: null,
-          userName: profile.username,
-          domain: null
-        });
-        newUser.save();
-        //console.log(newUser);
-        //return done(err, newUser);
-      }
-    })
-    return done(null, profile);
-  });
-}
-));
-
-
-
-
+//middleware
 app.use(bodyParser.json())
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
@@ -79,70 +30,11 @@ mongoose.connect("mongodb://localhost:27017/collab", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-app.use(partials());
-app.use(methodOverride());
 app.use(session({ secret: 'secret' ,resave:false,saveUninitialized:false,name:"sid",
 store: store,cookie: { maxAge: 1000 }}));
-app.use(passport.initialize());
-app.use(passport.session());
-//app.use(session({ secret: 'secret' ,resave:false,saveUninitialized:false,name:"sid"}));
-
-app.get("/",function(req,res){
-    //onst {userID}=req.session;
-    //console.log( req.session);
-    res.render('index');
-});
-
-app.get('/auth/github',
-  passport.authenticate('github', { scope: [ 'user:email' ] }),
-  function(req, res){
-    // The request will be redirected to GitHub for authentication, so this
-    // function will not be called.
-  });
-
-app.get('/signin', 
-  passport.authenticate('github', { failureRedirect: '/' }),
-  function(req, res) {
-    //console.log(req.session.passport.user);
-    res.redirect('/dashboard');
-  });
-
-app.get('/dashboard', ensureAuthenticated,function(req, res) {
+app.use("/",users);
 
 
-    User.findOne({userName:req.session.passport.user},function(err,foundUser){
-      if(foundUser)
-      {
-        res.render("dashboard",{username:foundUser.userName});
-      }
-    });
-    //res.render('dashboard');
-  });
-
-   app.get('/logout', function(req, res){
-
-    // req.session.destroy(function (err) {
-    //   res.clearCookie('connect.sid');
-    //   
-    //   res.redirect('/'); 
-    // });
-    req.logout();
-    req.session.destroy();
-    //cookie.Expires = DateTime.Now.AddDays(-1);
-    console.log(req.cookie);
-    res.redirect('/');
-  });
-  
-
-
-  function ensureAuthenticated(req, res, next) {
-    //console.log(req.session.passport.user);
-    if (req.isAuthenticated()) { return next(); }
-    res.redirect('/');
-    // if (req.session.passport.user) { return next(); }
-    // res.redirect('/');
-    
-  }
 app.listen(3000,function(){
     console.log("server started");
 });
---
+++ routes/users.js
@@ -0,0 +1,134 @@
+const express= require("express");
+const router = express.Router();
+const ejs=require("ejs");
+const session      = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+var methodOverride = require('method-override');
+var GitHubStrategy = require('passport-github2').Strategy;
+var partials = require('express-partials');
+var passport = require('passport');
+const mongoose=require("mongoose");
+require('dotenv').config()
+
+// requiring from different files
+const User = require("../models/user");
+
+
+//env variables
+const clientID=process.env.CLIENT_ID;
+const clientSecret=process.env.CLIENT_Secret;
+
+//passport used for github login authentication
+passport.serializeUser(function(user, done) {
+  //console.log(user.username);
+  done(null, user.username);
+});
+
+passport.deserializeUser(function(user, done) {
+  //console.log(user);
+  User.findOne({userName:user},function(err,user){
+    done(null, user);
+  });
+  
+});
+
+passport.use(new GitHubStrategy({
+  clientID: clientID,
+  clientSecret: clientSecret,
+  callbackURL: "http://localhost:3000/signin"
+},
+function(access_token, refreshToken, profile, done) {
+
+  // asynchronous verification, for effect...
+  process.nextTick(function () {
+    
+    // To keep the example simple, the user's GitHub profile is returned to
+    // represent the logged-in user.  In a typical routerlication, you would want
+    // to associate the GitHub account with a user record in your database,
+    // and return that user instead.
+    //console.log(User);
+    User.findOne({userName:profile.username},function(err,foundUser){
+      if(foundUser!=null){
+        //console.log(foundUser);
+        //return done(err, foundUser);
+      }
+      else{
+        let newUser = new User({
+          email: null,
+          userName: profile.username,
+          domain: null
+        });
+        newUser.save();
+        //console.log(newUser);
+        //return done(err, newUser);
+      }
+    })
+    return done(null, profile);
+  });
+}
+));
+
+//middleware
+router.use(partials());
+router.use(methodOverride());
+router.use(passport.initialize());
+router.use(passport.session());
+
+router.get("/",function(req,res){
+    //onst {userID}=req.session;
+    //console.log( req.session);
+    res.render('index');
+});
+
+router.get('/auth/github',
+  passport.authenticate('github', { scope: [ 'user:email' ] }),
+  function(req, res){
+    // The request will be redirected to GitHub for authentication, so this
+    // function will not be called.
+  });
+
+router.get('/signin', 
+  passport.authenticate('github', { failureRedirect: '/' }),
+  function(req, res) {
+    //console.log(req.session.passport.user);
+    res.redirect('/dashboard');
+  });
+
+router.get('/dashboard', ensureAuthenticated,function(req, res) {
+
+
+    User.findOne({userName:req.session.passport.user},function(err,foundUser){
+      if(foundUser)
+      {
+        res.render("dashboard",{username:foundUser.userName});
+      }
+    });
+    //res.render('dashboard');
+  });
+
+   router.get('/logout', function(req, res){
+
+    // req.session.destroy(function (err) {
+    //   res.clearCookie('connect.sid');
+    //   
+    //   res.redirect('/'); 
+    // });
+    req.logout();
+    req.session.destroy();
+    //cookie.Expires = DateTime.Now.AddDays(-1);
+    console.log(req.cookie);
+    res.redirect('/');
+  });
+  
+
+
+function ensureAuthenticated(req, res, next) {
+    //console.log(req.session.passport.user);
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/');
+    // if (req.session.passport.user) { return next(); }
+    // res.redirect('/');
+    
+  }
+
+  module.exports=router;
